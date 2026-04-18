99f7643b291e6e7c504c9edd7fecec80ea5b3bb7
jaydip.mer <jaydip.mer@bacancy.com>
2026-04-18 11:58:52 +0530

    - Working on the catalog service

---
+++ eshop-microservice/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Catalog.Api.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Categories,string Description, decimal Price, string ImageFile): ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            // Create Domain Entity
+
+            Product product = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Categories = request.Categories,
+                Description = request.Description,
+                Price = request.Price,
+                ImageFile = request.ImageFile
+            };
+
+            // Save to Database
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            // Return Result
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
--- eshop-microservice/Services/Catalog/Catalog.Api/Program.cs
+++ eshop-microservice/Services/Catalog/Catalog.Api/Program.cs
@@ -1,6 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCarter(new DependencyContextAssemblyCatalog(typeof(Program).Assembly));
+builder.Services.AddMediatR(configuration =>
+{
+    configuration.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(
+        builder.Configuration.GetConnectionString("Database")!
+    );
+
+    options.DatabaseSchemaName = "app";
+})
+.UseLightweightSessions();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
+app.MapCarter();
 app.Run();
