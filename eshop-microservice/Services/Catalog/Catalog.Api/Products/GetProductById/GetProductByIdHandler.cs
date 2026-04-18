bf09d5bf075347fb07db4d64e29cacab9fb48876
Jaydip Mer <jayvi1002@gmail.com>
2026-04-18 20:05:07 +0530

    - Added Get Products by Id endpoint

---
+++ eshop-microservice/Services/Catalog/Catalog.Api/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Catalog.Api.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product product);
+    internal class GetProductByIdQueryHandler(IDocumentSession session) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+            if(product == null)
+            {
+                throw new ArgumentNullException(nameof(product));
+            }
+            return new GetProductByIdResult(product);
+        }
+    }
+}
